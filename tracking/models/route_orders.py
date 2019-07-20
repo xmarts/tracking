b166413f90a595c9a726c4aa13266b24abb2b42b
Luis Muro <luis.muro@xmarts.com>
2019-07-20 02:06:50 -0500

    Campos de error

--- tracking/models/route_orders.py
+++ tracking/models/route_orders.py
@@ -28,12 +28,14 @@ class RouteOrder(models.Model):
         ('2', 'Pendiente'),
         ('3', 'Incompleta'),
         ('4', 'Completa'),
-        ('5', 'Cacelada')
+        ('5', 'Cancelada'),
+        ('11', 'Error')
         ], string='Estado', copy=False, default='0')
     sale_order_id = fields.Many2one('sale.order', string='Pedido de venta', required=False)
     stock_picking_id = fields.Many2one('stock.picking', string='Albaran de salida', required=False)
     amount = fields.Float(digits=(32, 32),string='Monto Debe',defualt=0.0)
     zone_id = fields.Many2one('res.zona', string="Zona")
+    sync_error = fields.Char(string="Sync Error", readonly=True)
 
 
     @api.model
